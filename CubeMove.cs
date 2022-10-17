047b5df33682578ffd4afad28f2d46b4b1853a38
Kkuji <danilostarkov5@gmail.com>
2022-10-17 16:49:24 +0300

    Corrected to int

--- CubeMove.cs
+++ CubeMove.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class CubeMove : MonoBehaviour
 {
-    private float startPositionZ;
-    private float currentSpeed;
-    private float currentDistance;
+    private int currentSpeed;
+    private int currentDistance;
 
     void Start()
     {
-        startPositionZ = this.transform.position.z;
         currentSpeed = CubeController.speedValue;
         currentDistance = CubeController.distanceValue;
     }
